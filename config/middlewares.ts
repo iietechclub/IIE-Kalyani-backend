98b2669104223e24f4c65037ffebf570eb38dc2e
Rahul Mondal <rahulml2505@gmail.com>
2025-11-18 15:05:59 +0530

    feat: implement unpublish prevention middleware

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -4,6 +4,10 @@ export default [
   "strapi::security",
   "strapi::cors",
   // 'strapi::poweredBy',
+
+  // Custom middleware to handle unpublish logic
+  "api::global.unpublish",
+
   "strapi::query",
   "strapi::body",
   "strapi::session",
---
+++ src/api/global/middlewares/unpublish.ts
@@ -0,0 +1,20 @@
+const preventedEndpoints = ["api::global.global"];
+
+export default () => async (ctx, next) => {
+  const { method, url } = ctx.request;
+
+  const isPostRequest = method === "POST";
+  const isUnblushingEndpoint = preventedEndpoints.some((e) =>
+    url.startsWith(`/content-manager/single-types/${e}/actions/unpublish`)
+  );
+
+  if (isPostRequest && isUnblushingEndpoint) {
+    ctx.throw(
+      403,
+      "This entry cannot be unpublished. It must always remain published."
+    );
+  }
+
+  // Continue with the next middleware
+  await next();
+};
